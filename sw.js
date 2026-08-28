ff36dba965d3fb787e78e1779907db7e95619fcc
luisbarenos-hub <luis.barenos@uniagustiniana.edu.co>
2026-08-28 12:26:35 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHE_NAME = 'memoria-pwa-v1';
+const CACHE_NAME = 'memoria-pwa-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/3612/3612580.png'
+  './manifest.json'
 ];
 
-// Guardar archivos en caché al instalar
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Responder desde la caché cuando no hay internet
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+      return response || fetch(e.request).catch(() => caches.match('./index.html'));
     })
   );
 });
